ee44782e8a8dadf2dbeeb404e6345d846aa69cf6
JosephSpraggon <joe_spraggon@hotmail.co.uk>
2021-04-27 21:13:24 +0100

    setup react native navigation & loaded  in homescreen

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('./assets/messy-kids-logo.png')}/>
+      <StatusBar style="auto" />
+      <TouchableOpacity  style={styles.button}>
+        <Text style={styles.titleFont}>
+          Activities
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity  style={styles.button}>
+        <Text style={styles.titleFont}>
+          Favourites
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function Activities({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Activities</Text>
+      <Button
+        title="Go to Activities... again"
+        onPress={() => navigation.push('Activities')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Messy Kids' }} />
+        <Stack.Screen name="Activities" component={Activities} options={{ title: 'Activities' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFECF9',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 400,
+    height: 400,
+    marginTop: 10,
+    marginLeft: 25
+  },
+  button: {
+    width: 350,
+    height: 75,
+    backgroundColor: '#f2dced',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 60,
+    borderColor: 'black',
+    borderWidth: 2,
+    marginTop: 50,
+    marginBottom: 20,
+    shadowColor: 'rgba(0,0,0, .4)', // IOS
+    shadowOffset: { height: 3, width: 3 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    elevation: 5, // Android
+  },
+  titleFont: {
+    fontSize: 50,
+    fontFamily: 'Quicksand-Regular',
+    paddingBottom: 10,
+  }
 });
